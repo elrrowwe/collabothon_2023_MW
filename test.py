cc9b016dafe01ff005d30a7935583478ea1537d2
elrrowwe <248607@edu.p.lodz.pl>
2023-10-21 11:58:41 +0200

    embedding function

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import os
 from os.path import dirname, join 
 from dotenv import load_dotenv
 from vecdb import VectorDatabase 
-
+#W6hkUqzTQWcL6IVw
 
 #.env adjustments
 dotenv_path = join(dirname(__file__), ".env")
--- vecdb.py
+++ vecdb.py
@@ -1,14 +1,19 @@
 import pinecone 
 from llama_index.embeddings import HuggingFaceEmbedding
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
 import os
 from os.path import dirname, join 
 from dotenv import load_dotenv
 import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
 
 #.env adjustments
 dotenv_path = join(dirname(__file__), ".env")
 load_dotenv(dotenv_path)
 PINECONE_API = os.environ.get("PINECONE_API")
+OPENAI_API = os.environ.get("OPENAI_API")
+
 
 #the database class
 class VectorDatabase:
@@ -20,7 +25,16 @@ class VectorDatabase:
         
         self.index_name = 'conv'
         self.index = pinecone.Index(self.index_name)
-        self.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
+        # self.embeddings = OpenAIEmbeddings(
+        #     model="text-embedding-ada-002",
+        #     openai_api_key=OPENAI_API
+        # )
+
+        # self.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
+
+        # self.embed_model = OpenAIEmbeddings(model="text-embedding-ada-002", openai_api_key=OPENAI_API)
+        
+        self.embed_model = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
 
     def add_to_db(self, ind: str):
          embedding = self._get_embedding(ind)
@@ -29,7 +43,9 @@ class VectorDatabase:
          ])
 
     def _get_embedding(self, text:str):
-        embedding = self.embed_model.get_text_embedding(text) 
+        # embedding = self.embed_model.embed_query(text)
+        embedding = self.embed_model.embed_query(text) 
+          
 
         return embedding
     
@@ -42,4 +58,4 @@ class VectorDatabase:
         
 def cossim(a, b):
     cos_sim = np.dot(a, b)/(np.linalg.norm(a)*np.linalg.norm(b))
-    return cos_sim 
+    return abs(cos_sim) 
